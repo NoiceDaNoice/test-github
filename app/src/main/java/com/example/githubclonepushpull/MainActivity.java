ce77e410d47db79f9af8552052451abbe04544e2
Gary Adam <jioe656@gmail.com>
2021-06-03 21:00:33 +0700

    joemama
    joemama

--- app/src/main/java/com/example/githubclonepushpull/MainActivity.java
+++ app/src/main/java/com/example/githubclonepushpull/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String joe="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
